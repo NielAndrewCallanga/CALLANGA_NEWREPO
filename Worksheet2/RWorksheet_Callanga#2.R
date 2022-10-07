fa91c8bd7f6b2e4bedd974baacb66670d581140a
Niel Andrew Callanga <andrew.nac81@gmail.com>
2022-10-07 13:18:57 +0800

    second commit

--- Worksheet2/RWorksheet_Callanga#2.R
+++ Worksheet2/RWorksheet_Callanga#2.R
@@ -6,18 +6,30 @@ install.packages("devtools")
   #1.a	Sequence from -5 to 5
   seq <- -5:5
   seq
+#Output
+  #[1] -5 -4 -3 -2 -1  0  1  2  3  4  5
+  #It will display negative and positive numbers and have a 0 at the center of it.
+  
+  
   
   #2.a	x <- 1:7. What will be the value of x?
   x <- 1:7
   x
+  #Output
+  #[1] 1 2 3 4 5 6 7
 
 #2 Create a vector using seq() function
   #a seq(1, 3, by=0.2) # specify step size
   #Write the R code and its output. Describe the output.
   
   seq(1,3,by= 0.2)
+  
+  #Output 
+  #1.0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0
+  #It increases its value by .2 until it reaches its maximum value at 3	
+  
 
-#3 3.	A factory has a census of its workers. There are 50 workers in total. The following list  shows their ages:
+#3 A factory has a census of its workers. There are 50 workers in total. The following list  shows their ages:
   workers_age <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27,
                22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 43, 53, 41, 51, 35,
                24,33, 41, 53, 40, 18, 44, 38, 41, 48, 27, 39, 19, 30, 61, 54, 58, 26,
@@ -25,11 +37,22 @@ install.packages("devtools")
 
   #3.a	Access 3rd element, what is the value?
     workers_age[3]
+    #Output
+    #[1] 22
   #3.b	Access 2nd and 4th element, what are the values?
     workers_age[2]
+    #Output
+    #[1] 28
     workers_age[4]
+    #Output
+    #[1] 36
+    
   #3.c	Access all but the 1st element is not included. Write the R code and its output.
     workers_age[2:50]
+    #Output:
+    #[1] 28 22 36 27 18 52 39 42 29 35 31 27 22 37 34 19 20 57 49 50 37 46 25 17 37 43 53 41 	51 35 	24 33
+    #[33] 41 53 40 18 44 38 41 48 27 39 19 30 61 54 58 26 18
+    
     
   
 #4 a-b Create a vector x <- c("first"=3, "second"=0, "third"=9). Then named the vector, names(x).
@@ -38,6 +61,10 @@ install.packages("devtools")
     x[c("first", "third")]
     names(x)
   
+    #Output:
+    #"first" "second" "third"
+    #It was only the characters that were displayed from the vector.
+    
  
     
     
@@ -46,9 +73,15 @@ install.packages("devtools")
     #a.	Modify 2nd element and change it to 0;
   x <- -3:2
   x
-  
+  #Output
+  # -3 -2 -1  0  1  2
   x[2] <- 0
   x
+  
+  #Output:
+  # -3  0 -1  0  1  2
+  #It displays a sequence of negative and positive numbers.
+  
 
 #6. The following data shows the diesel fuel purchased by Mr. Cruz.
   
@@ -59,12 +92,21 @@ install.packages("devtools")
   #a a.	Create a data frame for month, price per liter (php) and purchase-quantity (liter). Write the codes.
   data_frame <- data.frame(Month, Price_per_liter_php, Purchase_quantity_liter )
   data_frame
+  #Output
+  #Month Price_per_liter_php Purchase_quantity_liter
+  #1   Jan               52.50                      25
+  #2   Feb               57.25                      30
+  #3 March               60.00                      40
+  #4   Apr               65.00                      50
+  #5   May               74.25                      10
+  #6  June               54.00                      45
   
   #b b.	What is the average fuel expenditure of Mr. Cruz from Jan to June? Note: Use
   #weighted.mean(liter, purchase)
   
   weighted.mean(Price_per_liter_php, Purchase_quantity_liter)
-
+  #Output
+  #59.2625
   
 #7)	R has actually lots of built-in datasets. For example, the rivers data “gives the lengths (in miles) of 141 “major” rivers in North America, as compiled by the US Geological Survey”.
   
@@ -72,11 +114,13 @@ install.packages("devtools")
   data <- c(length(rivers), sum(rivers), mean(rivers), median(rivers), var(rivers),
             sd(rivers), min(rivers), max(rivers))
   data
+  #Output
+  #141.0000  83357.0000    591.1844    425.0000 243908.4086    493.8708    135.0000   	3710.0000
   
-  
-#88.	The table below gives the 25 most powerful celebrities and their annual pay as ranked by the editions of Forbes magazine and as listed on the Forbes.com website.
+#8.	The table below gives the 25 most powerful celebrities and their annual pay as ranked by the editions of Forbes magazine and as listed on the Forbes.com website.
   
   PowerRanking <- 1:25
+  
     #a.	Create vectors according to the above table. Write the codes.
   CelebrityName <- c("Tom Cruise", "Rolling Stones", "Oprah Winfrey", "U2", 
                      "Tiger Woods", "Steven Spielberg", "Howard Stern", "50 Cent", "Cast of the sopranos",
@@ -93,12 +137,47 @@ install.packages("devtools")
   #b	Modify the power ranking and pay of J.K. Rowling. Change power ranking to 15 and pay to 90. Write the codes and its output.
   PowerRanking [19] <- 15
   PowerRanking
+  #Output
+  # 1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 15 20 21 22 23 24 25
+  
   Pay [19] <- 90
   Pay
+  
+  #Output
+  #67  90 225 110  90 332 302  41  52  88  55  44  55  40 233  34  40  47  90  25  39  45  32  40
+  #31
+  
   #c	Interpret the data.
   Magazine_Ranking <- data.frame(PowerRanking, CelebrityName, Pay)
   Magazine_Ranking
   
+  #Output
+  #   PowerRanking        CelebrityName Pay
+ # 1             1           Tom Cruise  67
+ # 2             2       Rolling Stones  90
+ # 3             3        Oprah Winfrey 225
+ # 4             4                   U2 110
+ # 5             5          Tiger Woods  90
+ # 6             6     Steven Spielberg 332
+ # 7             7         Howard Stern 302
+ # 8             8              50 Cent  41
+ # 9             9 Cast of the sopranos  52
+ # 10           10            Dan Brown  88
+ # 11           11    Bruce Springsteen  55
+ # 12           12         Donald Trump  44
+ # 13           13         Muhammad Ali  55
+ # 14           14       Paul McCartney  40
+ # 15           15         George Lucas 233
+ # 16           16           Elton John  34
+ # 17           17      David Letterman  40
+ # 18           18       Phil Mickelson  47
+ # 19           15          J.K Rowling  90
+ # 20           20           Bradd Pitt  25
+ # 21           21        Peter Jackson  39
+ # 22           22      Dr. Phil McGraw  45
+ # 23           23            Jay Lenon  32
+ # 24           24          Celine Dion  40
+ # 25           25          Kobe Bryant  31
   
 
 
